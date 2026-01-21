d999dfd4d409269c7c1541a5f1f1a78342ee7dfe
despi4 <johapo62@gmail.com>
2026-01-21 22:43:55 +0500

    add: html

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ import express from "express";
 import { authRoutes } from "./routes/auth.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { profileRoutes } from "./routes/profile.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function createApp() {
     const app = express();
+
+    app.use(express.static(path.join(__dirname, "public")));
     
     app.use(express.json());
 
@@ -13,6 +20,11 @@ export function createApp() {
     app.use(authRoutes);
 
     app.use(profileRoutes);
+    
+    app.get("/", (req, res) => {
+        res.redirect("/login.html");
+    });
+
 
     app.use(errorHandler);
     return app;
